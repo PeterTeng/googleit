7b9d2b21f7ebc7bf18122d008fd903f64656fce7
DONGSHIH TENG <tou9702@gmail.com>
2016-11-14 14:04:10 +0900

    Add image search

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 var program = require('commander');
 program
   .arguments('<word>')
-  .option('-i, --image', 'Search Image on google')
+  .option('-i, --image', 'Search Image on Google')
+  .option('-n, --news', 'Search News on Google')
 
   .action(function(word) {
     var exec = require('child_process').exec;
     var cmd = "open https://google.com/#q=" + word;
 
+    let base = "open \'https://google.com/search?q="
     if (program.image) {
-      cmd = "open \'https://google.com/search?q=" + word + "&tbm=isch\'"
+      cmd = base + word + "&tbm=isch\'"
+      exec(cmd, function(error, stdout, stderr) {});
+    } else if (program.news) {
+      cmd = base + word + "&tbm=nws\'"
       exec(cmd, function(error, stdout, stderr) {});
     } else {
       exec(cmd, function(error, stdout, stderr) {});
