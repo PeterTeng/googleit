94c91f38e57d872af343fce394727c4cfd1a5dc5
DONGSHIH TENG <tou9702@gmail.com>
2016-11-14 13:37:54 +0900

    Add image option

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
 var program = require('commander');
 program
   .arguments('<word>')
+  .option('-i, --image', 'Search Image on google')
+
   .action(function(word) {
     var exec = require('child_process').exec;
     var cmd = "open https://google.com/#q=" + word;
 
-    exec(cmd, function(error, stdout, stderr) {
-      console
-      .log(
-          "Open Browser and search: " + word
-      );
-    });
+    if (program.image) {
+      cmd = "open \'https://google.com/search?q=" + word + "&tbm=isch\'"
+      exec(cmd, function(error, stdout, stderr) {
+        console
+        .log(
+            cmd
+        );
+      });
+    } else {
+      exec(cmd, function(error, stdout, stderr) {
+        console
+        .log(
+            "Open Browser and search: " + word
+        );
+      });
+    }
   })
  .parse(process.argv);
