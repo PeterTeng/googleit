f6f81117863d2f026df02cd5f676094aeed0dcec
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 01:44:13 +0900

    Adjust echo

--- check_version.sh
+++ check_version.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
+
 index_version=`cat index.js | grep version | grep -o "'.*'"| tr -d "'"`
 package_version=`perl -nle 'print $& if m{(?<="version": ")[^"]*}' package.json`
 
-echo "Version in index.js : $index_version"
-echo "Version in package.json : $package_version"
+current_version=`cat VERSION`
 
 published_latest_version=`npm info googleit version`
 
-echo "Version on npm : $published_latest_version"
+echo "                   Version on npm : $published_latest_version"
+echo "  Current version in version file : $current_version"
+echo "--------------------------------------------------"
+echo "              Version in index.js : $index_version"
+echo "          Version in package.json : $package_version"
+
+#
+# Basic CPU and Memory information
+#
+read -r -p "Are you going to publish? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY])
+      echo "You say yes"
+      ;;
+    *)
+      echo "You said no"
+      ;;
+esac
