21e13b666ab02e55d124f11460f833ae8386dc4a
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 03:10:38 +0900

    Add examples to help

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 #! /usr/bin/env node
 var program = require('commander');
+
+program
+  .on('--help', function(){
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ googleit weather-tomorrow');
+    console.log('    $ googleit -i cat');
+    console.log('    $ googleit -b harry-potter');
+    console.log('');
+});
+
 program
   .version('1.1.4')
   .arguments('<word>')
