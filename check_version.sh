5bc0ea585943a32d6e6f6175330429136b382ce1
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 01:19:05 +0900

    Print version on npm

--- check_version.sh
+++ check_version.sh
@@ -4,3 +4,7 @@ package_version=`perl -nle 'print $& if m{(?<="version": ")[^"]*}' package.json`
 
 echo "Version in index.js : $index_version"
 echo "Version in package.json : $package_version"
+
+published_latest_version=`npm info googleit version`
+
+echo "Version on npm : $published_latest_version"
