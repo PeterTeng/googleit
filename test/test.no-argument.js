7a64ddec9e7302075c65ad98d72ead285fa8bdbf
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 17:20:53 +0900

    Add option

--- test/test.no-argument.js
+++ test/test.no-argument.js
@@ -1,15 +1,19 @@
 var should = require('should');
 
 
-function testErrorMessage(error, stdout, stderr) {
+function testErrorMessage(error, stdout, stderr, option) {
   stdout.should.be.exactly('');
   should.exist(error.message);
-  error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n');
+  if ( option === 'noOption') {
+    error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n');
+  } else {
+    error.message.should.be.exactly('Command failed: node index.js -' + option + '\nPlease enter searh terms. "googleit <terms>"\n');
+  }
 }
 
 var exec = require('child_process').exec;
 var cmd = 'node index.js';
 
 exec(cmd, function(error, stdout, stderr) {
-  testErrorMessage(error, stdout, stderr);
+  testErrorMessage(error, stdout, stderr, 'noOption');
 })
