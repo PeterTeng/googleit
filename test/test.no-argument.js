8a9c66bc15fbaf02bd87568562c3b34df7f879ea
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 16:50:36 +0900

    Use function

--- test/test.no-argument.js
+++ test/test.no-argument.js
@@ -3,8 +3,12 @@ var should = require('should');
 var exec = require('child_process').exec;
 var cmd = 'node index.js';
 
-exec(cmd, function(error, stdout, stderr) {
+function testErrorMessage(error, stdout, stderr) {
   stdout.should.be.exactly('');
   should.exist(error.message);
-  error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n')
+  error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n');
+}
+
+exec(cmd, function(error, stdout, stderr) {
+  testErrorMessage(error, stdout, stderr);
 })
