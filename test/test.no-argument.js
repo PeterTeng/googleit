52d638f069b28d1f0b42d6a0baaf8ce0af7b1973
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 16:39:58 +0900

    Add no argument test

---
+++ test/test.no-argument.js
@@ -0,0 +1,12 @@
+var should = require('should');
+
+var exec = require('child_process').exec;
+var cmd = 'node index.js';
+
+hello = 'hello';
+
+exec(cmd, function(error, stdout, stderr) {
+  stdout.should.be.exactly('');
+  should.exist(error.message);
+  error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n')
+})
