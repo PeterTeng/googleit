987a3561b4b6f0c3ed7721238aacd9b14cf62f24
DONGSHIH TENG <tou9702@gmail.com>
2016-11-19 16:51:50 +0900

    Refactor

--- test/test.no-argument.js
+++ test/test.no-argument.js
@@ -1,13 +1,14 @@
 var should = require('should');
 
-var exec = require('child_process').exec;
-var cmd = 'node index.js';
 
 function testErrorMessage(error, stdout, stderr) {
   stdout.should.be.exactly('');
   should.exist(error.message);
   error.message.should.be.exactly('Command failed: node index.js\nPlease enter searh terms. "googleit <terms>"\n');
 }
+
+var exec = require('child_process').exec;
+var cmd = 'node index.js';
 
 exec(cmd, function(error, stdout, stderr) {
   testErrorMessage(error, stdout, stderr);
