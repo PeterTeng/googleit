49db075fa86a50fe165e0ebb4a91e3885c9f8b31
DONGSHIH TENG <tou9702@gmail.com>
2016-11-23 18:23:34 +0900

    jshint

--- test/test.unknown-options.js
+++ test/test.unknown-options.js
@@ -1,20 +1,20 @@
 var should = require('should');
 
 // Test -z option which is not exist in options
-var optionForTest = "-z"
+var optionForTest = "-z";
 function getUnknownOptionError(callback) {
   var exec = require('child_process').exec;
   var cmd = 'node index.js' + ' ' + optionForTest;
 
-  exec(cmd, function(error, stdout, stderr) {
-    result = error.message;
-    callback(result)
-  })
+  exec(cmd, function(error) {
+    var result = error.message;
+    callback(result);
+  });
 }
 var errorMatch = function(result) {
   result.includes('Command failed:').should.equal(true);
   result.includes('index.js' + ' ' + optionForTest).should.equal(true);
   result.includes('error: unknown option `' + optionForTest).should.equal(true);
-}
+};
 
 getUnknownOptionError(errorMatch);
